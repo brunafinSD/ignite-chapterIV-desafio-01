388902afdc0aa8d6c3163d904bcdaeeac2a10b8e
Bruna Nunes <bruna.f.n@hotmail.com>
2021-10-11 10:38:45 -0300

    Header - Web

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { Flex, Image } from '@chakra-ui/react';
+
+export function Header() {
+  return (
+    <Flex
+      as="header"
+      maxWidth={1440}
+      h="100"
+      justify="center"
+      align="center">
+      <Image
+        src="/images/Logo.svg"
+        alt="worldtrip"
+      />
+    </Flex>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Header } from '../components/Header'
 
 export default function Home() {
-  return <h1>worldtrip</h1>
+  return <Header />
 }
